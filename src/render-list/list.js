999d11e87c7fe7b581268f614e7d067082799d3e
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2019-05-20 21:44:56 -0300

    render list

---
+++ src/render-list/list.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import '../render-list/list.css';
+
+const todos = [
+    {
+        id: 1,
+        description: "Learn React",
+        done: false
+    },
+    {
+        id: 2,
+        description: "Learn JS",
+        done: false
+    },
+    {
+        id: 3,
+        description: "Learn how to use Gitub",
+        done: true
+    }
+];
+
+const TodoList = () => {
+    return (
+        <ul className="list">
+            {todos.map(item => (
+                <li key="item.id">
+                    <div>{item.description}</div>
+                </li>
+            ))}
+        </ul>
+    )
+};
+
+export default TodoList;
