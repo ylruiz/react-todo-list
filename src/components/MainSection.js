6083ae02843aa2f3b86651eb35cd920b0681f2cf
Yunet Luis Ruiz <yunet.lruiz94@gmail.com>
2019-06-19 21:53:19 +0200

    added TOGGLE_TODO at reducers/todos.js

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -1,15 +1,30 @@
 import React from 'react'
 
 const MainSection = (props) => {
+
+    const isCompleted = item => {
+        return item.done ? 'todo-completed' : 'todo'
+    }
+
+    const toggleTodo = item => {
+        props.onToggleTodo(item)
+    }
+
     return (
         <section>
             <ul>
                 {props.list.map(item => (
-                    <li key={item.id}>
-                        <label>
-                            {item.description}
-                        </label>
-                    </li>
+                    <label className={isCompleted(item)}>
+                        <input 
+                            type="checkbox"
+                            checked={item.done}
+                            onChange={ () => toggleTodo(item) } />
+                        <li key={item.id}>
+                            <label>
+                                {item.description}
+                            </label>
+                        </li>
+                    </label>
                 ))}
             </ul>
         </section>
